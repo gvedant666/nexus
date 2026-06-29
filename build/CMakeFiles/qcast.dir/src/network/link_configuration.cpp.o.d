290605c647301b2436c1332388e726aef598df11
gvedant666 <gvedant666@gmail.com>
2026-06-30 00:24:00 +0530

    Fix: Apply .gitignore and untrack excluded files

--- build/CMakeFiles/qcast.dir/src/network/link_configuration.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/qcast.dir/src/network/link_configuration.cpp.o: \
- /home/vedant/Code/qcast/src/network/link_configuration.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/qcast.dir/src/network/posix_link_configuration.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/qcast.dir/src/network/posix_link_configuration.cpp.o: \
- /home/vedant/Code/qcast/src/network/posix_link_configuration.cpp \
- /usr/include/stdc-predef.h
